6502714ddfa73897082ca2c9c07ee53f33c3bd5f
Cristianpmg <165740606+Cristianpmg@users.noreply.github.com>
2026-01-18 04:09:07 -0300

    Create 2_entrenamiento_modelo.sql

---
+++ 2_entrenamiento_modelo.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE MODEL `ventas_retail.modelo_forecast_ventas`
+OPTIONS(
+  model_type='ARIMA_PLUS',
+  time_series_timestamp_col='fecha',
+  time_series_data_col='total_ventas'
+) AS
+SELECT
+  fecha,
+  SUM(cantidad) as total_ventas
+FROM
+  `ventas_retail.historial_ventas_limpio`
+GROUP BY
+  fecha;
